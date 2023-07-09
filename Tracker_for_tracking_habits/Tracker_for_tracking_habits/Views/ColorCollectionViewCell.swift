d8435f72ef1aba6c5ab828a006fe3711448f4e2a
Roman <31464179+grand-roman@users.noreply.github.com>
2023-07-09 22:25:50 +0700

    upgrade views

---
+++ Tracker_for_tracking_habits/Tracker_for_tracking_habits/Views/ColorCollectionViewCell.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+final class ColorCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "ColorCollectionViewCell"
+
+    private let canvasView: UIView = {
+        let view = UIView()
+
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 8
+        view.widthAnchor.constraint(equalToConstant: 52).isActive = true
+        view.heightAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+
+        view.layer.borderWidth = 3
+        view.layer.borderColor = UIColor.ypWhiteDay.cgColor
+
+        return view
+    }()
+
+    private let colorView: UIView = {
+        let view = UIView()
+
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 8
+        view.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        view.heightAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        makeViewLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(color: UIColor) {
+        colorView.backgroundColor = color
+    }
+
+    func selectColor() {
+        canvasView.layer.borderColor = colorView.backgroundColor?.withAlphaComponent(0.3).cgColor
+    }
+
+    func deselectColor() {
+        canvasView.layer.borderColor = UIColor.ypWhiteDay.cgColor
+    }
+
+    private func makeViewLayout() {
+        contentView.addSubview(canvasView)
+        contentView.addSubview(colorView)
+
+        canvasView.translatesAutoresizingMaskIntoConstraints = false
+        colorView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            canvasView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            canvasView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+
+            colorView.centerXAnchor.constraint(equalTo: canvasView.centerXAnchor),
+            colorView.centerYAnchor.constraint(equalTo: canvasView.centerYAnchor)
+            ])
+    }
+}
---
+++ Tracker_for_tracking_habits/Tracker_for_tracking_habits/Views/PlaceholderView.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+final class PlaceholderView: UIView {
+
+    private let imageView: UIImageView = {
+        let image = UIImageView()
+
+        image.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        image.heightAnchor.constraint(equalTo: image.widthAnchor).isActive = true
+
+        return image
+    }()
+
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+
+        label.textColor = .ypBlackDay
+        label.font = .systemFont(ofSize: 12, weight: .medium)
+
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        makeViewLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(image: UIImage?, caption: String) {
+        imageView.image = image
+        captionLabel.text = caption
+    }
+
+    private func makeViewLayout() {
+        self.addSubview(imageView)
+        self.addSubview(captionLabel)
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        captionLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+
+            captionLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
+            captionLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8)
+            ])
+    }
+}
