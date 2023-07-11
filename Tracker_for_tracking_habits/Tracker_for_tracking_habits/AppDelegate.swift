77ed951c09cd72d4850d49243fbd223900c7393c
Roman <31464179+grand-roman@users.noreply.github.com>
2023-07-11 12:51:30 +0700

    update

--- Tracker_for_tracking_habits/Tracker_for_tracking_habits/AppDelegate.swift
+++ Tracker_for_tracking_habits/Tracker_for_tracking_habits/AppDelegate.swift
@@ -14,21 +14,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return container
     }()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+        window = UIWindow()
+
+        if OnboardingPageStorage.shared.isOnboardingPagePassed {
+            window?.rootViewController = MainTabBarController()
+        } else {
+            window?.rootViewController = OnboardingPageViewController(
+                transitionStyle: .scroll,
+                navigationOrientation: .horizontal
+            )
+        }
+        window?.makeKeyAndVisible()
+
         return true
     }
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-
-        let sceneConfiguration = UISceneConfiguration(name: "Main", sessionRole: connectingSceneSession.role)
-        sceneConfiguration.delegateClass = SceneDelegate.self
-        return sceneConfiguration
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-
-
-    }
-
-
 }
